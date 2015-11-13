ccb415a5a747b7a24764a1acb5f51ef586f56b8f
Manuel Maldonado <manuel.maldonado@lextrend.com>
2015-11-13 10:58:49 +0100

    Paperclip storage

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,10 +4,23 @@ class Attachment < ActiveRecord::Base
   belongs_to :event
 
   # Attachment
-  has_attached_file :file
+  has_attached_file :file,
+      :path => ":rails_root/public/system/:attachment/:id/:style/:normalized_file_name",
+      :url => "/system/:attachment/:id/:style/:normalized_file_name"
+
+  Paperclip.interpolates :normalized_file_name do |attachment, style|
+    attachment.instance.normalized_file_name
+  end
+
+
   validates_attachment_content_type :file, content_type: ["application/pdf"], message: I18n.t('backend.allowed_file_content_types')
 
   # Validations
   validates :title, presence: true
+
+  def normalized_file_name
+    "#{self.file_file_name.gsub( /[^a-zA-Z0-9_\.]/, '_')}"
+  end
+
 
 end
