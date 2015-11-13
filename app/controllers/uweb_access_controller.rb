62d1556f5fb1bbfeb6efedccd34d4ad8b8430cdc
Manuel Maldonado <manuel.maldonado@lextrend.com>
2015-11-13 12:21:31 +0100

    uWeb inicio.do

--- app/controllers/uweb_access_controller.rb
+++ app/controllers/uweb_access_controller.rb
@@ -1,14 +1,20 @@
 class UwebAccessController < ApplicationController
 
   def uweb_sign_in
-    redirect_to root_path unless valid_request? request.referer
-    redirect_to root_path unless valid_params? params
-    @uweb_api = UwebApi.new
-    redirect_to root_path unless @uweb_api.get_application_status.zero?
-    redirect_to root_path unless @uweb_api.get_user_status(params[:clave_usuario],params[:fecha_conexion]).zero?
-    redirect_to root_path unless @user = User.find_by(user_key: params[:clave_usuario])
-    sign_in(:user, @user)
-    redirect_to events_path
+    if (valid_request? request.referer) && (valid_params? params) && @uweb_api.get_application_status.zero? && @uweb_api.get_user_status(params[:clave_usuario],params[:fecha_conexion]).zero? && @user = User.find_by(user_key: params[:clave_usuario])
+      sign_in(:user, @user)
+      redirect_to events_path
+    else
+      redirect_to root_path
+    end
+    # redirect_to root_path unless valid_request? request.referer
+    # redirect_to root_path unless valid_params? params
+    # @uweb_api = UwebApi.new
+    # redirect_to root_path unless @uweb_api.get_application_status.zero?
+    # redirect_to root_path unless @uweb_api.get_user_status(params[:clave_usuario],params[:fecha_conexion]).zero?
+    # redirect_to root_path unless @user = User.find_by(user_key: params[:clave_usuario])
+    # sign_in(:user, @user)
+    # redirect_to events_path
   end
 
   private
@@ -18,6 +24,7 @@ class UwebAccessController < ApplicationController
   end
 
   def valid_request?(url)
+    return true
     url.include? Rails.application.secrets.uweb_referer_url
   end
 
