96ca61ea23099e70a54a4f29e6dbcdfa541635ef
Manuel Maldonado <manuel.maldonado@lextrend.com>
2015-11-29 18:49:57 +0100

    On demand changes 26/11/2015

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -12,7 +12,11 @@ class Attachment < ActiveRecord::Base
     attachment.instance.normalized_file_name
   end
 
-  validates_attachment_content_type :file, content_type: ["application/pdf"], message: I18n.t('backend.allowed_file_content_types')
+  validates_attachment_content_type :file, content_type: ['application/pdf','image/jpeg', 'image/png','application/txt',
+   'text/plain','application/pdf', 'application/msword', 'application/msexcel', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+   'application/vnd.oasis.opendocument.text','application/x-vnd.oasis.opendocument.text','application/rtf', 'application/x-rtf', 'text/rtf',
+   'text/richtext', 'application/doc', 'application/docx', 'application/xls', 'application/xlsx','application/x-soffice', 'application/octet-stream'],
+    message: I18n.t('backend.allowed_file_content_types')
 
   # Validations
   validates :title, presence: true
