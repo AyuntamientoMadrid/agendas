90cdb0ff864f513767bac44c6ffb8731f52c8d74
Manuel Maldonado <manuel.maldonado@lextrend.com>
2015-12-20 13:03:59 +0100

    Home page title font size

--- app/controllers/uweb_access_controller.rb
+++ app/controllers/uweb_access_controller.rb
@@ -8,14 +8,6 @@ class UwebAccessController < ApplicationController
     else
       redirect_to root_path
     end
-    # redirect_to root_path unless valid_request? request.referer
-    # redirect_to root_path unless valid_params? params
-    # @uweb_api = UwebApi.new
-    # redirect_to root_path unless @uweb_api.get_application_status.zero?
-    # redirect_to root_path unless @uweb_api.get_user_status(params[:clave_usuario],params[:fecha_conexion]).zero?
-    # redirect_to root_path unless @user = User.find_by(user_key: params[:clave_usuario])
-    # sign_in(:user, @user)
-    # redirect_to events_path
   end
 
   private
