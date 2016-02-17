577805b7e6492584b0c9d728829f054380dd4a82
Manuel Maldonado <manuel.maldonado@lextrend.com>
2016-02-17 18:48:42 +0100

    Add mb_chars to titleize in order to show properly holders name

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -19,11 +19,11 @@ class Holder < ActiveRecord::Base
   end
 
   def full_name
-    (self.first_name.to_s+' '+self.last_name.to_s).titleize
+    (self.first_name.to_s+' '+self.last_name.to_s).mb_chars.titleize.to_s
   end
 
   def full_name_comma
-    (self.last_name.to_s+', '+self.first_name.to_s).titleize
+    (self.last_name.to_s+', '+self.first_name.to_s).mb_chars.titleize.to_s
   end
 
   def current_position
