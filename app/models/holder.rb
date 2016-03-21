f98fdb64add51605aeda00229c1251667b0a4b6f
Manuel Maldonado <manuel.maldonado@lextrend.com>
2016-03-21 11:33:31 +0100

    Manage blank last names when importing from uWeb

--- app/models/holder.rb
+++ app/models/holder.rb
@@ -32,8 +32,9 @@ class Holder < ActiveRecord::Base
 
   def self.create_from_uweb(data)
     holder = Holder.find_or_initialize_by(user_key: data['CLAVE_IND'])
-    holder.first_name = data["NOMBRE_USUARIO"].gsub(/\s+/, "")
-    holder.last_name = data["APELLIDO1_USUARIO"].gsub(/\s+/, "")+' '+data["APELLIDO2_USUARIO"].gsub(/\s+/, "")
+    holder.first_name = data["NOMBRE_USUARIO"].split
+    holder.last_name = data["APELLIDO1_USUARIO"].split
+    holder.last_name += ' ' + data["APELLIDO2_USUARIO"].split if data["APELLIDO2_USUARIO"].present?
     holder
   end
 end
