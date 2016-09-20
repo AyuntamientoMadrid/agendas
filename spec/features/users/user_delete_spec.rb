7171ba3717a97c24b0e5ebabf66257d393a8c7f5
MaiteHdezRivas <maite.hernandez@gmail.com>
2016-09-20 14:30:06 +0200

    solve driver test and holder nil

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -1,24 +1,13 @@
-include Warden::Test::Helpers
-Warden.test_mode!
 
-feature 'User delete', :devise, :js do
-
-  after(:each) do
-    Warden.test_reset!
-  end
+feature 'User delete' do
 
   scenario 'user can not delete own account' do
     user = FactoryGirl.create(:user, :admin)
-    login_as(user, :scope => :user)
+    signin(user.email, "please123")
     visit users_path
     expect(page).to have_content I18n.t 'backend.users'
     page.click_link('',href: user_path(user))
-    page.driver.browser.switch_to.alert.accept
     expect(page).to have_content I18n.t 'backend.unable_to_perform_operation'
   end
 
 end
-
-
-
-
