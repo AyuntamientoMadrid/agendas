9357e61358deae6050f7d3b86ed2544138e45f57
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-14 11:30:09 +0100

    Fix some rubocop issues

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,17 @@
 # Capybara.asset_host = 'http://localhost:3000'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app,
+  Capybara::Poltergeist::Driver.new(
+    app,
     timeout: 1.minute,
-    inspector: true, # allows remote debugging by executing page.driver.debug
-    phantomjs_logger: File.open(File::NULL, "w"), # don't print console.log calls in console
+    # allows remote debugging by executing page.driver.debug
+    inspector: true,
+    # don't print console.log calls in console
+    phantomjs_logger: File.open(File::NULL, "w"),
+    # disable image loading
     phantomjs_options: ['--load-images=no', '--disk-cache=false'],
-    extensions: [File.expand_path("../phantomjs_ext/disable_js_fx.js", __FILE__)] # disable js effects
+    # disable js effects
+    extensions: [File.expand_path("../phantomjs_ext/disable_js_fx.js", __FILE__)]
   )
 end
 
