37e30ab616b292cec99d08a6c15bffaef70c4e6d
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-15 20:29:14 +0100

    Refactor attendee spec

--- spec/models/attendee_spec.rb
+++ spec/models/attendee_spec.rb
@@ -1,25 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Attendee, type: :model do
+describe Attendee do
+
+  let(:attendee) { build(:attendee) }
+
+  it "should be valid if name company and position present" do
+    expect(attendee).to be_valid
+  end
 
   it "should be invalid if no name" do
-    @attendee = FactoryGirl.build(:attendee, name: nil)
-    expect(@attendee).not_to be_valid
+    attendee.name = nil
+
+    expect(attendee).not_to be_valid
   end
 
   it "should be invalid if no company" do
-    @attendee = FactoryGirl.build(:attendee, company: nil)
-    expect(@attendee).not_to be_valid
+    attendee.company = nil
+
+    expect(attendee).not_to be_valid
   end
 
   it "should be invalid if no position" do
-    @attendee = FactoryGirl.build(:attendee, position: nil)
-    expect(@attendee).not_to be_valid
-  end
+    attendee.position = nil
 
-  it "should be valid if name company and position present" do
-    expect(FactoryGirl.build(:attendee)).to be_valid
+    expect(attendee).not_to be_valid
   end
-
 
 end
