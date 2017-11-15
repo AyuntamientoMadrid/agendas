68008710f753a17f97e44095f274da806c579d83
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-15 20:53:15 +0100

    Remove comments from attendee model

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,11 +1,8 @@
 class Attendee < ActiveRecord::Base
 
-  # Relations
   belongs_to :event
 
-  # Validations
   validates :name, presence: true
   validates :position, presence: true
   validates :company, presence: true
-
 end
