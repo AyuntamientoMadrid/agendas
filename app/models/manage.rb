a6b6d0c17480360c733b71d7e87068278a46e73f
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-15 22:47:17 +0100

    Remove comments and add basic manage model specs

--- app/models/manage.rb
+++ app/models/manage.rb
@@ -1,10 +1,4 @@
 class Manage < ActiveRecord::Base
-
-  # Relations
   belongs_to :user
   belongs_to :holder
-
-  # Validations
-  # validates_uniqueness_of :holder_id, scope: :user_id, message: I18n.t('backend.participants_uniqueness')
-
 end
