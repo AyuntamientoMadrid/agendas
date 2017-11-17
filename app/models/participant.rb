57a59bd0575059e871872cf18ab88f987b82e939
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-11-17 20:34:32 +0100

    Remove comments from participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,10 +1,4 @@
 class Participant < ActiveRecord::Base
-
-  # Relations
   belongs_to :position
   belongs_to :participants_event, class_name: "Event", foreign_key: "event_id"
-
-  # Validations
-  # validates_uniqueness_of :position_id, scope: :event_id
-
 end
