0c08685e29313fb9acaa000e0436148c2652601f
Bertocq <bertocq@gmail.com>
2017-11-21 00:17:07 +0100

    Add db:test_seeds spec

---
+++ spec/lib/tasks/db_rake_spec.rb
@@ -0,0 +1,16 @@
+describe "db:test_seeds" do
+  include_context "rake"
+
+  it "Generates dummy test data for local development" do
+    subject.invoke
+    expect(Area.count).to eq(3)
+    expect(User.count).to eq(4)
+    expect(Holder.count).to eq(4)
+    expect(Position.count).to eq(6)
+    expect(Event.count).to eq(4)
+    expect(Participant.count).to eq(5)
+    expect(Manage.count).to eq(2)
+    expect(Attendee.count).to eq(3)
+    expect(Attachment.count).to eq(2)
+  end
+end
