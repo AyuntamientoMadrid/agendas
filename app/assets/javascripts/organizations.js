9342411393530b9423117d67dc407c1d763ba16f
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-11-23 13:17:44 +0100

    Added style to form. Addded errors. Create partials.

--- app/assets/javascripts/organizations.js
+++ app/assets/javascripts/organizations.js
@@ -5,4 +5,49 @@ $(function(){
   $('#nested-legal-representant').bind("cocoon:after-remove", function() {
     $("#legal_representant_link").show();
   });
+
+  $(".organization-form").validate({
+      ignore: [],
+      errorElement: "small",
+      rules : {
+        "organization[name]" : {
+            required : true
+        },
+        "organization[email]" : {
+            required : true
+        },
+        "organization[user_attributes][password]" : {
+            required : true
+        },
+        "organization[first_name]" : {
+            required : true
+        },
+        "organization[last_name]" : {
+            required : true
+        }
+      },
+
+      messages : {
+        "organization[name]" : {
+            required : "Añade el nombre"
+        },
+        "organization[email]" : {
+            required : "Añade email"
+        },
+        "organization[user_attributes][password]" : {
+            required : "Añade contraseña"
+        },
+        "organization[first_name]" : {
+            required : "Añade nombre"
+        },
+        "organization[last_name]" : {
+            required : "Añade apellido"
+        }
+      },
+
+      errorPlacement : function(error, element) {
+          error.insertAfter(element);
+      }
+  });
+
 });
