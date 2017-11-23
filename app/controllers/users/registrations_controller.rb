532aacc7274efef863a41ae138c35c1616036f4d
iagirre <itziar.agirre@m2c.es>
2017-11-23 10:09:19 +0100

    Admin layout when the password couldn't be saved (due to erros in form data).

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # before_filter :configure_sign_up_params, only: [:create]
 # before_filter :configure_account_update_params, only: [:update]
 
-  layout "admin", only: [:edit]
+  layout "admin"
 
   # GET /resource/sign_up
   # def new
