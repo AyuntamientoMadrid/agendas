5b32e99d64793742bf8ee907e5f22aaa0d15aa96
iagirre <itziar.agirre@m2c.es>
2017-11-24 08:27:47 +0100

    Added seeds for organization categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+names = ['Consultoría profesional y despachos de abogados', 'Empresas', 'Asociaciones/Fundaciones',
+         'Sindicatos y organizaciones profesionales', 'Organizaciones empresariales',
+         'ONGs y plataformas sin personalidad jurídica',
+         'Universidades y centros de investigación',
+         'Corporaciones de Derecho Público (colegios profesionales, cámaras oficiales, etc.)',
+         'Iglesia y otras confesiones', 'Otro tipo de sujetos']
+
+names.each do |name|
+  Category.find_or_create_by(name: name)
+end
