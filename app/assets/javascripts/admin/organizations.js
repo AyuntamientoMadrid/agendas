03dbe426e1ff9e9fe396b6cd8ab50c34c6aabae0
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-11-27 16:14:48 +0100

    Added id to nested agents and represened entities via js. Remove tag id form html.

--- app/assets/javascripts/admin/organizations.js
+++ app/assets/javascripts/admin/organizations.js
@@ -16,20 +16,17 @@ $(function(){
   }
 
   var agents_i = 0;
-
   $('#nested-agents').on('cocoon:after-insert', function() {
     agents_i ++;
-    $('#new_agent').attr("id",'new_agent_' + agents_i);
+    $(this).children().last().attr('id','new_agent_' + agents_i);
   });
 
   var represented_i = 0;
   $('#nested-represented-entities').on('cocoon:after-insert', function() {
     represented_i ++;
-    $('#new_represented_entity').attr("id",'new_represented_entity_' + represented_i);
+    $(this).children().last().attr('id','new_represented_entity_' + represented_i);
   });
-
-
-
+  
 });
 
 function restore_legal_representant(e){
