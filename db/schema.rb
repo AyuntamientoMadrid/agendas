38a53105deba02a055dc0c255fc7330eaf68269b
Angel Perez <iAngel.p93@gmail.com>
2017-11-30 09:14:09 -0400

    Migrate DB to include 'deleted_at' attribute into 'users'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129141137) do
+ActiveRecord::Schema.define(version: 20171130131121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -280,6 +280,7 @@ ActiveRecord::Schema.define(version: 20171129141137) do
     t.string   "user_key"
     t.string   "phones"
     t.integer  "organization_id"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
