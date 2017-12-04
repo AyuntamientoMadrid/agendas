c81ae60d669a6d01df575e4efd06e0d523ecc825
iagirre <itziar.agirre@m2c.es>
2017-12-04 12:47:22 +0100

    Add migration to change the events status column type. Add enum to event model to set the status that will be used.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203204027) do
+ActiveRecord::Schema.define(version: 20171204113941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,12 +112,12 @@ ActiveRecord::Schema.define(version: 20171203204027) do
     t.text     "description"
     t.datetime "scheduled"
     t.integer  "user_id"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.integer  "position_id"
     t.string   "location"
     t.string   "slug"
-    t.string   "status"
+    t.integer  "status",            default: 0
     t.boolean  "lobby_activity"
     t.text     "notes"
     t.string   "reasons"
