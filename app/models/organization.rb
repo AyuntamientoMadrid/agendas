87f9cd678968f3de91c8ac8448d534cd667ca4c7
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-12-08 15:44:48 +0100

    Translate range and true value.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,7 +3,6 @@ class Organization < ActiveRecord::Base
   enum registered_lobbies: [:no_record, :generalitat_catalunya, :cnmc, :europe_union, :others]
   enum range_fund: [:range_1, :range_2, :range_3, :range_4]
   enum entity_type: [:association, :federation, :lobby]
-
   validates :inscription_reference, uniqueness: true, allow_blank: true, allow_nil: true
   validates :name, :category_id, presence: true
 
