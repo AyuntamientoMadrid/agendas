204fcb3c84ec07900541cfe5bc509ea38dcfa356
taitus <sebastia.roig@gmail.com>
2017-12-10 18:59:24 +0100

    Only display default categories on organization#new

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205234013) do
+ActiveRecord::Schema.define(version: 20171210174630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 20171205234013) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "display"
   end
 
   create_table "event_agents", force: :cascade do |t|
