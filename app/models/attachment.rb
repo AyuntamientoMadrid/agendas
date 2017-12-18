d103416b5a2e25526d399745e4d5e9dc31e4f016
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-12-18 17:57:12 +0100

    Fix some rubocop offenses

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -7,14 +7,14 @@ class Attachment < ActiveRecord::Base
       :url => "/system/:attachment/:id/:style/:normalized_file_name"
 
   validates :title, :file, presence: true
-  validates_inclusion_of :public, in: [true, false]
-  validates_attachment_content_type :file, content_type: [
-    'application/pdf','image/jpeg', 'image/png','application/txt',
-    'text/plain', 'application/msword', 'application/msexcel',
-    'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.oasis.opendocument.text',
-    'application/x-vnd.oasis.opendocument.text','application/rtf', 'application/x-rtf', 'text/rtf', 'text/richtext',
-    'application/doc', 'application/docx', 'application/xls', 'application/xlsx','application/x-soffice', 'application/octet-stream'],
+  validates :public, inclusion: [true, false]
+  validates_attachment_content_type :file, content_type:
+    ['application/pdf','image/jpeg', 'image/png','application/txt',
+     'text/plain', 'application/msword', 'application/msexcel',
+     'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+     'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.oasis.opendocument.text',
+     'application/x-vnd.oasis.opendocument.text','application/rtf', 'application/x-rtf', 'text/rtf', 'text/richtext',
+     'application/doc', 'application/docx', 'application/xls', 'application/xlsx', 'application/x-soffice', 'application/octet-stream'],
     message: I18n.t('backend.allowed_file_content_types')
 
   Paperclip.interpolates :normalized_file_name do |attachment, style|
