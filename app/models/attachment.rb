2c850f54e263d4c2c813332448eb606f00019061
taitus <sebastia.roig@gmail.com>
2017-12-18 21:41:01 +0100

    Fix duplicated error on attachment validations

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -17,12 +17,23 @@ class Attachment < ActiveRecord::Base
      'application/doc', 'application/docx', 'application/xls', 'application/xlsx', 'application/x-soffice', 'application/octet-stream'],
     message: I18n.t('backend.allowed_file_content_types')
 
+    after_validation :cleanup_paperclip_duplicate_errors
+
   Paperclip.interpolates :normalized_file_name do |attachment, style|
     attachment.instance.normalized_file_name
   end
 
   def normalized_file_name
     "#{self.file_file_name.gsub( /[^a-zA-Z0-9_\.]/, '_')}"
+  end
+
+  private
+
+  # This is because paperclip duplicates error messages
+  # See: https://github.com/thoughtbot/paperclip/pull/1554 and
+  # https://github.com/thoughtbot/paperclip/commit/2aeb491fa79df886a39c35911603fad053a201c0
+  def cleanup_paperclip_duplicate_errors
+    errors.delete(:file)
   end
 
 end
