66dd203811198336e371423411686893fbd94b93
taitus <sebastia.roig@gmail.com>
2017-12-22 18:37:15 +0100

    Display text by current user on events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,4 +8,20 @@ module EventsHelper
     !event.canceled? && !event.declined? && !event.accepted?
   end
 
+  def event_title(current_user)
+    if current_user.lobby?
+      t("backend.title_event_lobby")
+    else
+      t("backend.title_event")
+    end
+  end
+
+  def event_new(current_user)
+    if current_user.lobby?
+      t("backend.new_requeseted_event")
+    else
+      t("backend.new_event")
+    end
+  end
+
 end
