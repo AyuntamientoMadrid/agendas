b3b62d7c27b131afbec9d745cb6c3104cbc081fd
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-12-27 00:58:45 +0100

    Add abilities specs

--- spec/models/abilities/lobby_spec.rb
+++ spec/models/abilities/lobby_spec.rb
@@ -4,19 +4,30 @@ require 'cancan/matchers'
 feature "Abilities::User lobby" do
   let(:lobby_user)    { create(:user, role: :lobby) }
   let!(:organization) { create(:organization, user: lobby_user) }
-  let!(:agent)                      { create(:agent, organization: organization) }
-  let!(:another_organization_agent) { create(:agent) }
 
   subject(:ability) { Ability.new(lobby_user) }
 
-  it { should be_able_to(:index, Agent) }
-  it { should be_able_to(:new, Agent) }
+  describe "Agents" do
+    let!(:agent)                      { create(:agent, organization: organization) }
+    let!(:another_organization_agent) { create(:agent) }
 
-  it { should be_able_to(:edit, agent) }
-  it { should be_able_to(:update, agent) }
-  it { should_not be_able_to(:destroy, agent) }
+    it { should be_able_to(:index, Agent) }
+    it { should be_able_to(:new, Agent) }
+    it { should be_able_to(:edit, agent) }
+    it { should be_able_to(:update, agent) }
+    it { should_not be_able_to(:destroy, agent) }
 
-  it { should_not be_able_to(:edit, another_organization_agent) }
-  it { should_not be_able_to(:update, another_organization_agent) }
-  it { should_not be_able_to(:destroy, another_organization_agent) }
+    it { should_not be_able_to(:edit, another_organization_agent) }
+    it { should_not be_able_to(:update, another_organization_agent) }
+    it { should_not be_able_to(:destroy, another_organization_agent) }
+  end
+
+  describe "OrganizationInterests" do
+    let!(:organization_interest)      { create(:organization_interest, organization: organization) }
+    let!(:another_organization_organization_interest) { create(:organization_interest) }
+
+    it { should be_able_to(:index, OrganizationInterest) }
+    it { should be_able_to(:update, organization_interest) }
+    it { should_not be_able_to(:update, another_organization_organization_interest) }
+  end
 end
