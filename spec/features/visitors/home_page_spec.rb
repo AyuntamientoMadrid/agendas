9077d590b3cff24fdaa9fa08dd1773b456d4908f
Vicente Mendoza <jose.vicente.mendoza@m2c.es>
2017-12-26 16:24:04 +0100

    add website map

--- spec/features/visitors/home_page_spec.rb
+++ spec/features/visitors/home_page_spec.rb
@@ -6,4 +6,10 @@ feature 'Home page' do
     expect(page).to have_content I18n.t 'main.title'
   end
 
+  scenario 'Should redirect to Website map', :search do
+    visit root_path
+    find(:xpath, "/html/body/footer/div[2]/div/div[2]/ul/li[3]/a").click
+    expect(page).to have_content I18n.t 'websitemap.title'
+  end
+
 end
