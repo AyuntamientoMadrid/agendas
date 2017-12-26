ad05d21fa36bffc73e233823d949f2d8210cbf80
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-12-26 11:49:46 +0100

    Fix error

--- app/helpers/admin/agents_helper.rb
+++ app/helpers/admin/agents_helper.rb
@@ -1,17 +1,19 @@
-module AgentsHelper
-  def agent_attachment_persisted_data(attachment)
-    return "data-persisted=true" if attachment.persisted?
-  end
+module Admin
+  module AgentsHelper
+    def agent_attachment_persisted_data(attachment)
+      return "data-persisted=true" if attachment.persisted?
+    end
 
-  def agent_attachment_persisted_remove_notice(attachment)
-    return if attachment.blank?
-    render partial: 'layouts/admin_messages',
-           locals: {
-             alert: t("backend.agents.attachment_destroy_notice")
-           }
-  end
+    def agent_attachment_persisted_remove_notice(attachment)
+      return if attachment.blank?
+      render partial: 'layouts/admin_messages',
+             locals: {
+               alert: t("backend.agents.attachment_destroy_notice")
+             }
+    end
 
-  def agents_public_assignments_editor(agent)
-    agent.new_record? ? "mceNoEditor" : "tinymce"
+    def agents_public_assignments_editor(agent)
+      agent.new_record? ? "mceNoEditor" : "tinymce"
+    end
   end
 end
