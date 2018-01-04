44dec487ba9ad97a3671cdb33152cbf192625634
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-01-04 16:52:21 +0100

    Changed Organization.lobby.all.map with Organization.lobby.map from statistics_helper.

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,6 +1,6 @@
 module StatisticsHelper
   def self_employed(value)
-    hash_employed = Organization.lobby.all.map { |i| [i.name, i.represented_entities.count] }.to_h
+    hash_employed = Organization.lobby.map { |i| [i.name, i.represented_entities.count] }.to_h
     ret = if value
             hash_employed.select { |_key, amount| amount > 0 }
           else
