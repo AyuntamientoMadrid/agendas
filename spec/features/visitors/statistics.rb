62beec2cd86f7460dbf94c9afaa7b3c28ff44745
Vicente Mendoza <jose.vicente.mendoza@m2c.es>
2018-01-04 17:18:49 +0100

    Fixed suggestions

--- spec/features/visitors/statistics.rb
+++ spec/features/visitors/statistics.rb
@@ -2,9 +2,17 @@ feature 'Statistics page' do
 
   scenario 'view statistics page' do
     visit statistics_path
-    expect(page).to have_content I18n.t 'statistics.statistics'
     expect(page).to have_content I18n.t 'statistics.description'
     expect(page).to have_content I18n.t 'statistics.category_lobbies'
   end
 
+ scenario 'view items on statistics page' do
+    create(:event, published_at: Time.zone.yesterday)
+    create(:interest , name: "Houses")
+    visit statistics_path
+    within "#public_agendas" do
+      expect(page).to have_content "1"
+    end
+    expect(page).to have_content "Houses"
+  end
 end
