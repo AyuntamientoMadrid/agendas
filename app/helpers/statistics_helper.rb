fb52eb5759f66fa00124eeabbdc979720e2b3fb5
María Checa <MariaCheca@users.noreply.github.com>
2018-04-26 16:05:31 +0200

    Fixed methods to retrieve inscribed lobbies data in stats view

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,11 +1,9 @@
 module StatisticsHelper
   def employee_lobbies_count(lobbies)
-    hash_employed = lobbies.map { |l| [l.name, l.represented_entities.count] }.to_h
-    hash_employed.select { |_key, amount| amount > 0 }.count
+    lobbies.select { |l| l.employee_lobby }.count
   end
 
   def self_employed_lobbies_count(lobbies)
-    hash_employed = lobbies.map { |i| [i.name, i.represented_entities.count] }.to_h
-    hash_employed.select { |_key, amount| amount.zero? }.count
+    lobbies.select { |l| l.self_employed_lobby }.count
   end
 end
