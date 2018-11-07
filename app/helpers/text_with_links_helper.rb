39d3c0a21991a12f0fc77fa657f8d8dcc1886032
voodoorai2000 <voodoorai2000@gmail.com>
2018-11-07 11:51:25 +0100

    Remove unused return statement in html helper

--- app/helpers/text_with_links_helper.rb
+++ app/helpers/text_with_links_helper.rb
@@ -3,7 +3,6 @@ module TextWithLinksHelper
 
   def safe_html_with_links(html)
     return if html.nil?
-    return html.html_safe unless html.html_safe?
     Rinku.auto_link(html, :all, 'target="_blank" rel="nofollow"').html_safe
   end
 
