e7bf8cb4b9c35b32828cc8d267d863393ace10bb
voodoorai2000 <voodoorai2000@gmail.com>
2018-11-07 12:26:30 +0100

    Remove rspec focus configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -61,5 +61,4 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-  config.filter_run :focus => true
 end
