14972d3decc677b3fe1d525b0931b9e1d130d56d
voodoorai2000 <voodoorai2000@gmail.com>
2018-11-16 17:25:22 +0100

    Fix specs
    Add mailer_helper[1] to devise mailer, as both the user_mailer and devise_mailer use the same layout which required a helper method
    
    [1] https://github.com/AyuntamientoMadrid/agendas/pull/493/commits/4464570dc50dbcfba380dc9f925bf0fbc1fb8687#diff-6338cac8af5d0dac3dd6cf3183d97ce1R1

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,3 +260,7 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 end
+
+Devise::Mailer.class_eval do
+  helper :mailer
+end
